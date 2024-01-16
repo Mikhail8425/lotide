5010a08874ea0dceadb9786326dbc253a722652a
Mikhail8425 <mikhailshemet@gmail.com>
2024-01-16 17:34:38 -0500

    head refactor

--- test/headTest.js
+++ test/headTest.js
@@ -1,23 +1,21 @@
-const assert = require("../assertEqual");
+const assert = require("chai").assert;
 const head = require("../head");
 
-describe('head', () => {
+describe("#head", () => {
   // 1
   it("returns 1 for [1, 2, 3]", () => {
-    const inputName = [1, 2, 3];
-    const expectedOutput = 1;
-    assert(head(inputName), expectedOutput);
+    assert.strictEqual(head([1, 2, 3]), 1);
   });
   // 2
   it("returns '5' for ['5']", () => {
-    const inputName = ["5"];
-    const expectedOutput = "5";
-    assert(head(inputName), expectedOutput);
+    assert.strictEqual(head(["5"]), "5");
   });
   // 3
   it("returns '5' for ['5, 6, 7']", () => {
-    const inputName = [5, 6, 7];
-    const expectedOutput = 5;
-    assert(head(inputName), expectedOutput);
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  // 4
+  it("returns '1' for ['1, 6, 12']", () => {
+    assert.strictEqual(head([1, 6, 12]), 1);
   });
 });
--- test/middleTest.js
+++ test/middleTest.js
@@ -6,18 +6,18 @@ describe('middle', () => {
   it("returns [] for [2]", () => {
     const inputName = [2];
     const expectedOutput = [];
-    assert(middle(inputName), expectedOutput);
+    assert(middle(inputName) === expectedOutput);
   });
   // 2
   it("returns [] for [1, 2]", () => {
     const inputName = [1, 2];
     const expectedOutput = [];
-    assert(middle(inputName), expectedOutput);
+    assert(middle(inputName) === expectedOutput);
   });
   // 3
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
     const inputName = [1, 2, 3, 4, 5];
     const expectedOutput = [3];
-    assert(middle(inputName), expectedOutput);
+    assert(middle(inputName) === expectedOutput);
   });
 });
