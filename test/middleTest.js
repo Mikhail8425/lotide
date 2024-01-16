9ee6343703500c1a87289c25bce361788b55da94
Mikhail8425 <mikhailshemet@gmail.com>
2024-01-16 17:28:01 -0500

    middle + test

---
+++ test/middleTest.js
@@ -0,0 +1,23 @@
+const assert = require("../assertArraysEqual");
+const middle = require("../middle");
+
+describe('middle', () => {
+  // 1
+  it("returns [] for [2]", () => {
+    const inputName = [2];
+    const expectedOutput = [];
+    assert(middle(inputName), expectedOutput);
+  });
+  // 2
+  it("returns [] for [1, 2]", () => {
+    const inputName = [1, 2];
+    const expectedOutput = [];
+    assert(middle(inputName), expectedOutput);
+  });
+  // 3
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    const inputName = [1, 2, 3, 4, 5];
+    const expectedOutput = [3];
+    assert(middle(inputName), expectedOutput);
+  });
+});
